3b21a4ad2879923b1075f6c7242eacdd57ad4c78
Anastasia-cher <114093927+Anastasia-cher@users.noreply.github.com>
2022-10-26 14:06:31 +0300

    add hw 1

---
+++ HW 1/Простые числа/Прстые числа.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <cmath> 
+
+bool simple(int x) { 
+    for (int divider = 2; divider <= pow(x, 0.5); ++divider) {
+        if (x % divider == 0) {
+            return 0;
+        }
+    }
+}
+
+void main() {
+    int maxNumber;
+    printf("Enter max number: ");
+    scanf_s("%d", &maxNumber);
+    printf("\n");
+    for (int numder = 2; numder <= maxNumber; ++numder) {
+        if (simple(numder)) {
+            printf("%d\n", numder);
+        }
+    }
+}
