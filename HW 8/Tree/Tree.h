b22eb56ae47937a8953be8a535daa8d0b0f4edfe
Anastasia-cher <114093927+Anastasia-cher@users.noreply.github.com>
2022-11-02 15:10:40 +0300

    Create Tree.h

---
+++ HW 8/Tree/Tree.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+struct Tree;
+
+
+Tree* createTree();
+// Создание дерева
+// функция возвращает указатель на дерево.
+
+void addValue(Tree* tree, int key, char* value);
+// Функция добавляет значение в дерево
+
+char* getValue(Tree* tree, int key);
+// Функция возвращает копию значения по ключу
+
+bool contains(Tree* tree, int key);
+// Проверка наличия ключа
+
+void deleteValue(Tree* tree, int key);
+// Удаление значения по ключу
+
+void deleteTree(Tree* tree);
+// Удаление дерева
