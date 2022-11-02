6c8fda9987e067f1ff746f978e45e0384d83975f
Anastasia-cher <114093927+Anastasia-cher@users.noreply.github.com>
2022-11-02 14:29:38 +0300

    Check sorted array

--- HW 3/Поиск/Поиск.c
+++ HW 3/Поиск/Поиск.c
@@ -32,3 +32,53 @@ void qsort(int array[], int left, int right) {
 		qsort(array, pivot, right);
 	}
 }
+
+bool checkSortedArray(int array[], int lengthOfArray)
+{
+	for (int i = 0; i < lengthOfArray - 1; i++)
+	{
+		if (array[i] > array[i + 1])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+bool tests()
+{
+	bool testsPassed = true;
+	const int length = 10;
+	int testArray1[length] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	qsort(testArray1, 0, length - 1);
+	if (!checkSortedArray(testArray1, length))
+	{
+		printf("Error in test with array already sorted\n");
+		testsPassed = false;
+	}
+
+	int testArray2[length] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+	qsort(testArray2, 0, length - 1);
+	if (!checkSortedArray(testArray2, length))
+	{
+		printf("Error in test with all the identical elements\n");
+		testsPassed = false;
+	}
+
+	int testArray3[length] = { 1, 4, 10, 6, 4, 7, 11, 1, 4, 5 };
+	qsort(testArray3, 0, length - 1);
+	if (!checkSortedArray(testArray3, length))
+	{
+		printf("Error when pivot is minimum in array\n");
+		testsPassed = false;
+	}
+
+	int testArray4[length] = { 15, 4, 10, 6, 7, 15, 11, 3, 4, 5 };
+	qsort(testArray4, 0, length - 1);
+	if (!checkSortedArray(testArray4, length))
+	{
+		printf("Error when pivot is maximum in array");
+		testsPassed = false;
+	}
+	return testsPassed;
+}
