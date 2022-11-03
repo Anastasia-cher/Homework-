0933ccdead6bd21ec6e3c471da1ef782c6113da8
Anastasia-cher <114093927+Anastasia-cher@users.noreply.github.com>
2022-11-03 19:21:47 +0300

    modules and files

---
+++ HW 4/Modules and files/Modules and files.cpp
@@ -0,0 +1,26 @@
+﻿#include <climits>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "qsort.h"
+#include "Search.h"
+
+bool checkSortedArray(int array[], int lengthOfArray) {
+	for (int i = 0; i < lengthOfArray - 1; i++) {
+		if (array[i] > array[i + 1]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool testForQsort() {
+	const int length = 13;
+	int array[length] = {};
+	srand(time(nullptr));
+	for (int i = 0; i < length; i++) {
+		array[i] = rand() % 10;
+	}
+	qsort(array, 0, length - 1);
+	return checkSortedArray(array, length);
+}
