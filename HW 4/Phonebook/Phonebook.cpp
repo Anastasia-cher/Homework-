b9d8838d5535e44a80b9c33d622f33f0a5e8398f
Anastasia-cher <114093927+Anastasia-cher@users.noreply.github.com>
2022-11-03 19:22:17 +0300

    Update Phonebook.cpp

--- HW 4/Phonebook/Phonebook.cpp
+++ HW 4/Phonebook/Phonebook.cpp
@@ -8,14 +8,11 @@ struct Book {
     char phone[50];
 };
 
-int countEntriesInBuffer(Book entries[], char const fileName[])
-{
+int countEntriesInBuffer(Book entries[], char const fileName[]) {
 	FILE* file = fopen(fileName, "a");
 	int entriesCounter = 0;
-	while (!feof(file))
-	{
-		if (fscanf_s(file, "%[^-]%*c%*c", entries[entriesCounter].name) != EOF)
-		{
+	while (!feof(file)) {
+		if (fscanf_s(file, "%[^-]%*c%*c", entries[entriesCounter].name) != EOF) {
 			const int length = strlen(entries[entriesCounter].name);
 			entries[entriesCounter].name[length - 1] = '\0';
 			fscanf_s(file, "%[^\n]%*c", entries[entriesCounter].phone);
@@ -35,4 +32,28 @@ void being() {
     printf(" 3 - найти номер телефона по имени\n");
     printf(" 4 - найти имя по номеру телефону\n");
     printf(" 5 - сохранить текущие данные\n");
+}
+
+void addEntry(Book entries[], int &amountOfEntriesInBuffer) {
+	if (amountOfEntriesInBuffer < 100) {
+		printf("Введите имя: ");
+		scanf_s("\n%[^\n]", entries[amountOfEntriesInBuffer].name);
+		printf("Введите номер телефона: ");
+		scanf_s("\n%[^\n]", entries[amountOfEntriesInBuffer].phone);
+		++amountOfEntriesInBuffer;
+	}
+	else {
+		printf("В базе данных недостаточно место\n");
+	}
+}
+
+void printAllEntries(Book entries[], int amountOfEntriesInBuffer) {
+	if (amountOfEntriesInBuffer == 0) {
+		printf("Записей нет\n");
+	}
+	else {
+		for (int i = 0; i < amountOfEntriesInBuffer; ++i) {
+			printf("%s - %s\n", entries[i].name, entries[i].phone);
+		}
+	}
 }
