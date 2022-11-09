b84081746b4f4357917a2d8d94f0e65b8b294da0
Anastasia-cher <114093927+Anastasia-cher@users.noreply.github.com>
2022-11-09 18:10:08 +0300

    КР

---
+++ HW 6/Postfix Calculator/temp.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include "customStack.h"
+#include <malloc.h>
+
+int main() {
+    Stack* stack = createStack();
+    push(stack, 'a');
+    printf("%c", top(stack));
+
+    free(stack);
+    return 0;
+}
