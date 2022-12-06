ac850e48a237eb20fae9e2f9f2ddb368e432fbca
Anastasia-cher <114093927+Anastasia-cher@users.noreply.github.com>
2022-12-06 15:21:18 +0300

    fixed the task with prime numbers in the first homework

--- HW 1/Простые числа/Простые числа.c
+++ HW 1/Простые числа/Простые числа.c
@@ -2,22 +2,26 @@
 #include <stdlib.h>
 #include <cmath> 
 
-bool simple(int x) { 
+bool prime(int x) { 
     for (int divider = 2; divider <= pow(x, 0.5); ++divider) {
         if (x % divider == 0) {
-            return 0;
+            return true;
+        }
+        else {
+            return false
         }
     }
 }
 
 void main() {
-    int maxNumber;
+    int maxNumber = 0;
     printf("Enter max number: ");
     scanf_s("%d", &maxNumber);
     printf("\n");
-    for (int numder = 2; numder <= maxNumber; ++numder) {
-        if (simple(numder)) {
-            printf("%d\n", numder);
+    for (int number = 2; number <= maxNumber; ++number) {
+        if (prime(number)) {
+            printf("%d\n", number);
         }
     }
+    return 0;
 }
