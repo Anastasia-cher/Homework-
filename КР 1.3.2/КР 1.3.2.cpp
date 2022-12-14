b99939987151c2cc85fe0244bdaf1377c3b12200
Anastasia-cher <114093927+Anastasia-cher@users.noreply.github.com>
2022-12-14 18:48:01 +0300

    Create КР 1.3.2.cpp

---
+++ КР 1.3.2/КР 1.3.2.cpp
@@ -0,0 +1,62 @@
+﻿#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <locale.h>
+
+struct Numbers {
+    int number[100];
+};
+
+
+void being() {
+    setlocale(LC_ALL, "Rus");
+    printf("\nВведите:\n 0 - выйти\n");
+    printf(" 1 – ввести число;\n");
+    printf(" 2 – распечатать пять самых больших из введённых до сих пор чисел.\n");
+}
+
+void addNumber(Numbers entries[], int& amountOfEntriesInBuffer) {
+    if (amountOfEntriesInBuffer < 100) {
+        printf("\nВведите число: ");
+        scanf_s("%d", entries[amountOfEntriesInBuffer].number);
+        ++amountOfEntriesInBuffer;
+    }
+    else {
+        printf("В базе данных недостаточно место\n");
+    }
+}
+
+void printNumbers(Numbers entries[], int& amountOfEntriesInBuffer) {
+    if (amountOfEntriesInBuffer == 0) {
+        printf("Записей нет\n");
+    }
+    else {
+        int* max = 0;
+        for (int i = 0; i < amountOfEntriesInBuffer; ++i) {
+            if (entries[amountOfEntriesInBuffer].number > max) {
+                max = entries[amountOfEntriesInBuffer].number;
+            }
+        }
+        for (int i = 0; i < 5; ++i) {
+            printf("%d", entries[amountOfEntriesInBuffer].number);
+        }
+    }
+}
+
+int main() {
+    being();
+    int amountOfEntriesInBuffer = 0;
+    int N = 0;
+    scanf_s("%d", &N);
+    switch (N) {
+    case 0:
+        break;
+    case 1:
+        addNumber(number[100], amountOfEntriesInBuffer);
+        break;
+    default:
+        printNumbers(number[100], amountOfEntriesInBuffer);
+            break;
+    }
+    return 0;
+}
