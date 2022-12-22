2819c1059e096b99215a7cc41fe0bb911c488c37
Anastasia-cher <114093927+Anastasia-cher@users.noreply.github.com>
2022-12-22 17:34:11 +0300

    hw 10

---
+++ HW 10/Hash table.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+//a wrapper for Node
+typedef struct List List;
+
+//function that creates a linked list
+List* createList();
+
+//inserts a new element to the beginning of the list
+void listInsertBeginning(char* word, List* List);
+
+//returns the value of the frequency field
+int getFrequency(List* List);
+
+//returns the value of the word field
+const char* getWord(List* List);
+
+//moves head forward by one element
+bool listTraverse(List* List);
+
+//cheeks to see if the word is in the list
+bool isInTheList(List* List, char* word);
+
+//increments a frequency by 1
+void incrementFrequency(List* List, char* word);
+
+//returns the length of the list
+int listLength(List* List);
+
+//sets "newFrequency" elements with the "word" field
+void conveyFrequency(List* List, char* word, int newFrequency);
+
+//checks to see if the head points to NULL
+bool isEmpty(List* List);
---
+++ HW 10/MainHashTable.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include "Hash table.h"
+
+#define MODULE 2
+
+int main(void) {
+    FILE* file = fopen("file.txt", "r");
+    int module = MODULE;
+    HashTable* hashTable = createHashTable(MODULE);
+    char buffer[256];
+    while (!feof(file)) {
+        fscanf_s(file, "%s", buffer);
+        char* word = (char*)malloc(sizeof(char) * strlen(buffer));
+        strcpy(word, buffer);
+        hashInsert(hashTable, word, module);
+        if (getLoadFactor(hashTable) > 1) {
+            hashResize(&hashTable, &module);
+            updateLoadFactor(hashTable);
+        }
+    }
+    printWords(hashTable);
+    printf("\nThe load factor of the Hash Table is %f\n", getLoadFactor(hashTable));
+    printf("The length of the longest list in the hash table is %d\n", findMaxList(hashTable));
+    printf("The length of the average list in the hash table is %f\n", findAverageList(hashTable));
+    fclose(file);
+    return 0;
+}
